7bfc225622487aff284f21520cf9b3cd36fd9eed
Tess Lameyer <tjlameyer@gmail.com>
2019-09-18 20:57:23 -0700

    first css and js files

---
+++ app.js
@@ -0,0 +1,77 @@
+
+function showAlert() {  // funtion; some code that you can call later; i only want it when the button has been clicked 
+    alert('you clicked the button');
+}
+
+function logHello() {
+    console.log('hello');
+}
+
+// // alert('hello world')
+// typeof Array()
+
+// const dog = {     // dog is an object
+//     color: 'greenish blue',
+//     name: 'rover', 
+//     weight: 500,
+// }
+
+// //change dog name 
+
+// dog.name = 'spot' 
+
+// const myVegetables = ['broccoli', 'potato', 'carrot']
+
+// myVegetables[1] // will return potato 
+
+// let color = 'blue'
+// color = 'green'
+// const flavor = 'spicy'
+// // flavor const = 'savory' --> will not work 
+
+// let drink = 'water'
+// drink = drink + 's' --> output will be 'waters'
+
+// const dog = {
+//     name: 'fido', 
+//     color: 'brown',
+//     weight: 500,
+// };
+
+// console.log(dog.name);
+
+// dog.name = 'rover'; // reassigned to rover
+// console.log(dog.name);
+
+
+
+// if(dog.name === 'rover') {   // practice with control flow 
+//     console.log('red rover, come over')
+// } else if(dog.name === 'spot'){
+//     console.log('spot is a normal name')
+// } else if(dog.name === 'timothy'){
+//     console.log('that is a person name')
+// } else {
+//     console.log('you are just a normal dog')
+// };
+
+// // count up to ten, first time using for loop 
+
+// for (let i = 0; i <=10; i++) { // start at 0, stop when get to 10, go up by 1s
+//     console.log(i); // will print out 0 1 2 3 4 5 6 7 8 9 10 
+// }
+
+// // what if we have an array 
+
+// const veggies = ['carrot', 'potato', 'turnip']
+// for (let i = 0; i <=10; i++) { // start at 0, stop when get to 10, go up by 1s
+//     console.log(i); // will print out 0 1 2 3 4 5 6 7 8 9 10 
+//     console. log(veggies[i]); // will print out carrot potato turnip
+// } // actual print is 0 carrot 1 potato etc. ... 
+
+// //with control flow 
+
+// for (let i=0; i <=5; i++) {
+//     if(i === 2) {
+//         console.log(veggies[i])
+// }
